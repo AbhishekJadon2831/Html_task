518f05853655e9f3a4bd7200d2aaaf167ce6d7b3
AbhishekJadon2831 <abhishekjadon616@gmail.com>
2024-10-23 10:32:55 -0700

    Add files via upload

---
+++ New folder (3)/asseta/index3.js
@@ -0,0 +1,17 @@
+document.getElementById("bar").addEventListener("click", () => {
+    let heat2 = document.getElementById("heat2");
+    if (heat2 .style.display == "none") {
+        heat2 .style.display = "block"
+    } else {
+        heat2 .style.display = "none"
+    }
+})
+
+document.getElementById("bar4").addEventListener("click", () => {
+    let heat = document.getElementById("heat");
+    if (heat .style.display == "none") {
+        heat .style.display = "block"
+    } else {
+        heat .style.display = "none"
+    }
+})
